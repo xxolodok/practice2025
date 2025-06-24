76d1476f3431de95153a36c87dbf3b71bd5c6c01
Xterri-prog <matveik157@gmail.com>
2025-06-24 22:04:45 +0600

    feat(task01): добавлена функция-расширения String.IsPalindrome

---
+++ task01/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace task01
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+
+            var filteredInputString = new string(input
+                .ToLowerInvariant()
+                .Where(c => !char.IsPunctuation(c) && !char.IsWhiteSpace(c))
+                .ToArray());
+
+            string ReversedfilteredInputString = "";
+
+            for (int i = 0 ; i < filteredInputString.Length; i++)
+                ReversedfilteredInputString += filteredInputString[filteredInputString.Length - 1 - i];
+            
+            return filteredInputString.SequenceEqual(ReversedfilteredInputString);
+        }
+    }
+}
