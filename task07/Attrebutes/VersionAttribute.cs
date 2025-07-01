18eec195ca4509240c74fe23db44677411ed5ffe
Xterri-prog <matveik157@gmail.com>
2025-07-01 13:36:06 +0600

    feat(task07): добавлено задание 7

---
+++ task07/Attrebutes/VersionAttribute.cs
@@ -0,0 +1,18 @@
+namespace Attributes;
+
+[System.AttributeUsage(
+    System.AttributeTargets.All,
+    Inherited = false,
+    AllowMultiple = true
+)]
+public sealed class VersionAttribute : System.Attribute
+{
+    public int Major { get; } 
+    public int Minor { get; } 
+
+    public VersionAttribute(int Major, int Minor)
+    {
+        this.Major = Major;
+        this.Minor = Minor;
+    }
+}
---
+++ task07/ReflectionHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using Attributes;
+
+namespace task07
+{
+    public static class ReflectionHelper
+    {
+        public static void PrintTypeInfo(Type type)
+        {
+            var displayName = type.GetCustomAttribute<DisplayNameAttribute>();
+            Console.WriteLine($"Отображаемое имя класса: {(displayName != null ? displayName.DisplayName : "не указано имя")}");
+
+            var version = type.GetCustomAttribute<VersionAttribute>();
+            Console.WriteLine($"Версия класса: {(version != null ? $"{version.Major}.{version.Minor}" : "версия указана")}");
+
+            // Вывод информации о методах
+            PrintMembersInfo(type.GetMethods(BindingFlags.Public | BindingFlags.Instance), "Методы");
+
+            // Вывод информации о свойствах
+            PrintMembersInfo(type.GetProperties(), "Свойства");
+        }
+
+        private static void PrintMembersInfo(MemberInfo[] members, string title)
+        {
+            Console.WriteLine($"\n{title}:");
+
+            foreach (var member in members)
+            {
+                var displayName = member.GetCustomAttribute<DisplayNameAttribute>();
+                Console.WriteLine($"  {member.Name}: {(displayName != null ? displayName.DisplayName : "нет описания")}");
+            }
+        }
+    }
+}
