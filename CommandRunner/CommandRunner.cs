5c25f21579c9482d4b0583dd0a4fc6df2341f24e
Xterri-prog <matveik157@gmail.com>
2025-07-07 21:09:38 +0600

    feat(tests): добавлен тест для CommandRunner

---
+++ CommandRunner/CommandRunner.cs
@@ -0,0 +1,87 @@
+﻿using CommandLib;
+using System.Reflection;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace CommandRunner;
+
+public static class CommandRunner
+{
+    static public void Main()
+    {
+        bool shouldContinue = true;
+        while (shouldContinue)
+        {
+            Console.WriteLine("Выберите команду");
+            Console.WriteLine("1. Вычислить размер директории\n2.Найти файл\n3.Выход");
+            var option = int.TryParse(Console.ReadLine(), out int result);
+            if (option)
+            {
+                switch (result)
+                {
+                    case 1:
+                        InvokeDirectorySizeCommand();
+                        break;
+                    case 2:
+                        InvokeFindFilesCommand();
+                        break;
+                    case 3:
+                        Console.WriteLine("Произошел выход из программы");
+                        shouldContinue = false;
+                        break;
+                    default:
+                        Console.WriteLine("Неверный выбор");
+                        break;
+                }
+                ;
+            }
+        }
+    }
+    private static void InvokeDirectorySizeCommand()
+    {
+        string testDir = CreateDirectory();
+
+        Assembly assembly = LoadAssembly();
+
+        Type commandType = assembly.GetType("FileSystemCommands.DirectorySizeCommand");
+        object commandInstance = Activator.CreateInstance(commandType, testDir);
+        MethodInfo executeMethod = commandType.GetMethod("Execute");
+        executeMethod.Invoke(commandInstance, null);
+        RemoveDirectory(testDir);
+    }
+
+    private static void InvokeFindFilesCommand()
+    {
+        Console.WriteLine("Введите маску для поиска файла или оставьте пустой");
+        string? Mask = Console.ReadLine();
+        string testDir = CreateDirectory();
+
+        Assembly assembly = LoadAssembly();
+
+        var commandType = assembly.GetType("FileSystemCommands.FindFilesCommand");
+        var commandInstance = Activator.CreateInstance(commandType, new[] { testDir, Mask });
+        var executeMethod = commandType.GetMethod("Execute");
+        executeMethod.Invoke(commandInstance, null);
+        RemoveDirectory(testDir);
+    }
+    private static Assembly LoadAssembly()
+    {
+        string assemblyPath = "C:/practice2025/FileSystemCommands/bin/Debug/net9.0/FileSystemCommands.dll";
+        Assembly assembly = Assembly.LoadFile(assemblyPath);
+        return assembly;
+    }
+    private static string CreateDirectory()
+    {
+        var testDir = Path.Combine(Path.GetTempPath(), "TestDir");
+
+        Directory.CreateDirectory(testDir);
+        File.WriteAllText(Path.Combine(testDir, "test1.txt"), "12345"); 
+        File.WriteAllText(Path.Combine(testDir, "test2.txt"), "67890"); 
+
+        return testDir;
+    }
+    private static void RemoveDirectory(string testDir)
+    {
+        Directory.Delete(testDir, recursive: true);   
+    }
+}
--- task08tests/FileSystemCommandsTests.cs
+++ task08tests/FileSystemCommandsTests.cs
@@ -1,12 +1,27 @@
-﻿using task08;
-using FileSystemCommands;
+﻿using FileSystemCommands;
+using CommandRunner;
 using CommandLib;
 using Xunit;
 using System.IO;
 using System;
+using Moq;
 
 public class FileSystemCommandsTests
 {
+
+    private readonly Mock<TextReader> _mockInput;
+    private readonly Mock<TextWriter> _mockOutput;
+    private readonly StringWriter _outputWriter;
+
+    public FileSystemCommandsTests()
+    {
+        _mockInput = new Mock<TextReader>();
+        _mockOutput = new Mock<TextWriter>();
+        _outputWriter = new StringWriter();
+        Console.SetOut(_mockOutput.Object);
+        Console.SetIn(_mockInput.Object);
+    }
+
     [Fact]
     public void DirectorySizeCommand_ShouldCalculateSize()
     {
@@ -18,7 +33,7 @@ public class FileSystemCommandsTests
 
 
         var command = new DirectorySizeCommand(testDir);
-        
+
         using var consoleOutput = new StringWriter();
         Console.SetOut(consoleOutput);
 
@@ -43,7 +58,7 @@ public class FileSystemCommandsTests
 
 
         var command = new FindFilesCommand(testDir, "*.txt");
-        
+
         using var consoleOutput = new StringWriter();
         Console.SetOut(consoleOutput);
 
@@ -58,6 +73,82 @@ public class FileSystemCommandsTests
 
 
         Directory.Delete(testDir, true);
+    }
+
+    [Fact]
+    public void Main_ShouldExit_WhenOption3Selected()
+    {
+        var inputSequence = new Queue<string>();
+        inputSequence.Enqueue("3");
+
+        _mockInput.SetupSequence(x => x.ReadLine())
+            .Returns(inputSequence.Dequeue());
+
+        CommandRunner.CommandRunner.Main();
+
+        _mockOutput.Verify(x => x.WriteLine(It.Is<string>(s => s.Contains("Произошел выход из программы"))));
+    }
+
+    [Fact]
+    public void Main_ShouldShowError_WhenInvalidOptionSelected()
+    {
+        var inputSequence = new Queue<string>();
+        inputSequence.Enqueue("99");
+        inputSequence.Enqueue("3");
+
+        _mockInput.SetupSequence(x => x.ReadLine())
+            .Returns(inputSequence.Dequeue())
+            .Returns(inputSequence.Dequeue());
+
+
+        CommandRunner.CommandRunner.Main();
+
+        _mockOutput.Verify(x => x.WriteLine(It.Is<string>(s => s.Contains("Неверный выбор"))));
+    }
+
+    [Fact]
+    public void Main_ShouldShowError_WhenFirstOptiionSelected()
+    {
+        var inputSequence = new Queue<string>();
+        inputSequence.Enqueue("99");
+        inputSequence.Enqueue("1");
+        inputSequence.Enqueue("3");
+
+        _mockInput.SetupSequence(x => x.ReadLine())
+            .Returns(inputSequence.Dequeue())
+            .Returns(inputSequence.Dequeue())
+            .Returns(inputSequence.Dequeue());
+
+
+        CommandRunner.CommandRunner.Main();
+
+
+        _mockOutput.Verify(x => x.WriteLine(It.Is<string>(s => s.Contains("Размер директории: 10 байтов"))));
+    }
+
+
+    [Fact]
+    public void Main_ShouldInvokeFindFiles_WhenOption2Selected()
+    {
+        // Arrange
+        var inputSequence = new Queue<string>();
+        inputSequence.Enqueue("2");
+        inputSequence.Enqueue("*.txt"); 
+        inputSequence.Enqueue("3");
+
+        _mockInput.SetupSequence(x => x.ReadLine())
+            .Returns(inputSequence.Dequeue())
+            .Returns(inputSequence.Dequeue())
+            .Returns(inputSequence.Dequeue());
+
+
+        CommandRunner.CommandRunner.Main();
+
+        _mockOutput.Verify(x => x.WriteLine(It.Is<string>(s =>
+            s.Contains("Введите маску для поиска файла или оставьте пустой"))));
+        _mockOutput.Verify(x => x.WriteLine(It.Is<string>(s =>
+            s.Contains("найден") && s.Contains("файл"))));
+
     }
 }
 
