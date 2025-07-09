3b72b83d7166fbf3272b30eff9f0b6ed75080513
Xterri-prog <matveik157@gmail.com>
2025-07-09 15:17:28 +0600

    feat(CodeCompilerTests): Написан тест для CodeCompiler

---
+++ task11tests/CodeCompilerTests.cs
@@ -0,0 +1,25 @@
+﻿using task11;
+
+namespace task11tests;
+
+public class CodeCompilerTests
+{
+    [Fact]
+    public void CodeCompilerTests_ReturnCorrectInstance()
+    {
+        string code = @"public class Calculator
+                        {
+                            public int Add(int a, int b) => a + b;
+                            public int Minus(int a, int b) => a - b;
+                            public int Mul(int a, int b) => a * b;
+                            public int Div(int a, int b) => a / b;
+                        }";
+        dynamic instance = CodeCompiler.CodeGenerate(code);
+
+        Assert.NotNull(instance);
+        Assert.Equal(5, instance.Add(2, 3));
+        Assert.Equal(-1, instance.Minus(2, 3));
+        Assert.Equal(6, instance.Mul(2, 3));
+        Assert.Equal(2, instance.Div(6, 3));
+    }
+}
