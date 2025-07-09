39f7ea6cfb4fe596cdde987a0ed0ff657e90d49c
Xterri-prog <matveik157@gmail.com>
2025-07-09 14:44:10 +0600

    feat(Plagin): Создана система зависимостей тестовых плагинов, где PluginTwo зависит от PluginOne. Файлы сборок плагинов сохраняются в папку PluginLib

---
+++ PluginOne/PluginOne.cs
@@ -0,0 +1,13 @@
+﻿using AttributesLib;
+using Interfaces;
+
+namespace PluginOne;
+
+[PluginLoad]
+public class PluginOne : IPlugin
+{
+    public void Execute()
+    {
+        Console.WriteLine("PluginOne is executing!");
+    }
+}
---
+++ PluginTwo/PluginTwo.cs
@@ -0,0 +1,13 @@
+﻿using AttributesLib;
+using Interfaces;
+using PluginOne;
+namespace PluginTwo;
+
+[PluginLoad(typeof(PluginOne.PluginOne))]
+public class PluginTwo : IPlugin
+{
+    public void Execute()
+    {
+        Console.WriteLine("PluginTwo is executing!");
+    }
+}
