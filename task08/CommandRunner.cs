e6d6a13ea355eb6a6798ceaf965f6cb370ff0e47
Xterri-prog <matveik157@gmail.com>
2025-07-12 01:12:53 +0600

    тестовый PR

--- task08/CommandRunner.cs
+++ task08/CommandRunner.cs
@@ -41,9 +41,9 @@ public static class CommandRunner
 
         Assembly assembly = LoadAssembly();
 
-        Type commandType = assembly.GetType("FileSystemCommands.DirectorySizeCommand")!;
-        object commandInstance = Activator.CreateInstance(commandType, testDir)!;
-        MethodInfo executeMethod = commandType.GetMethod("Execute")!;
+        Type commandType = assembly.GetType("FileSystemCommands.DirectorySizeCommand");
+        object commandInstance = Activator.CreateInstance(commandType, testDir);
+        MethodInfo executeMethod = commandType.GetMethod("Execute");
         executeMethod.Invoke(commandInstance, null);
     }
 
@@ -55,16 +55,15 @@ public static class CommandRunner
 
         Assembly assembly = LoadAssembly();
 
-        Type? commandType = assembly.GetType("FileSystemCommands.FindFilesCommand");
-        var commandInstance = Activator.CreateInstance(commandType!, new[] { testDir, Mask });
-        var executeMethod = commandType!.GetMethod("Execute");
-        executeMethod?.Invoke(commandInstance, null);
+        var commandType = assembly.GetType("FileSystemCommands.FindFilesCommand");
+        var commandInstance = Activator.CreateInstance(commandType, new[] { testDir, Mask });
+        var executeMethod = commandType.GetMethod("Execute");
+        executeMethod.Invoke(commandInstance, null);
     }
     private static Assembly LoadAssembly()
     {
         string assemblyPath = "C:/practice2025/FileSystemCommands/bin/Debug/net9.0/FileSystemCommands.dll";
-        var normalizedPath = Path.GetFullPath(assemblyPath.Replace('/', Path.DirectorySeparatorChar));
-        var assembly = Assembly.LoadFile(normalizedPath);
+        Assembly assembly = Assembly.LoadFile(assemblyPath);
         return assembly;
     }
     private static string CreateDirectory()
