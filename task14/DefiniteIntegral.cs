166ce72ff910133a4dffbbf6fb4b02e38ba46351
Xterri-prog <matveik157@gmail.com>
2025-07-15 20:36:19 +0600

    feat(task03): Написаны тесты и реализация программы для вычилсения опредленного интеграла методом трапеций

---
+++ task14/DefiniteIntegral.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Linq;
+
+namespace task14;
+
+public class DefiniteIntegral
+{
+    public static double Solve(double a, double b, Func<double, double> function,
+                              double step, int threadsNumber)
+    {
+        double totalResult = 0.0;
+        double intervalLength = b - a;
+        double subIntervalLength = intervalLength / threadsNumber;
+
+        Thread[] threads = new Thread[threadsNumber];
+
+        var barrier = new Barrier(threadsNumber + 1);
+        threads = Enumerable.Range(0, threadsNumber).Select(i =>
+        {
+            double start = a + i * subIntervalLength;
+            double end = (i == threadsNumber - 1) ? b : start + subIntervalLength;
+
+            var thread = new Thread(() =>
+            {
+                double localResult = 0.0;
+                double x = start;
+
+                while (x < end)
+                {
+                    double nextX = Math.Min(x + step, end);
+                    double y1 = function(x);
+                    double y2 = function(nextX);
+
+                    localResult += (y1 + y2) * (nextX - x) / 2;
+                    x = nextX;
+                }
+
+                double current, newValue;
+                current = totalResult;
+                newValue = current + localResult;
+                Interlocked.CompareExchange(ref totalResult, newValue, current);
+
+                barrier.SignalAndWait();
+            });
+
+            return thread;
+        }).ToArray();
+
+        foreach (var thread in threads)
+        {
+            thread.Start();
+        }
+
+        barrier.SignalAndWait();
+        barrier.Disponse();
+
+        return totalResult;
+    }
+}
